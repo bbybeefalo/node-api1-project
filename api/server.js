9d64d4e54baf4a83f374bcea4378a2451c0faa84
Morgan Frotten <mmfrotten@gmail.com>
2023-01-04 09:42:44 -0500

    get

--- api/server.js
+++ api/server.js
@@ -1,3 +1,32 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express');
+const User = require('../api/users/model');
+const server = express();
+server.use(express.json());
+
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json({ message: 'something went wrong' })
+    }
+})
+
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await User.findById(id)
+        if (!user) {
+            res.status(404).json( {message: `Error retrieving user with ID ${id}`})
+        } else { 
+             res.status(200).json(user)
+        }
+       
+    } catch (err) {
+        res.status(500).json({ message: `Error fetching user ${req.params.id}`})
+    }
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
