1496fc68aee87b8ee02c13b1937a22b153115738
Morgan Frotten <mmfrotten@gmail.com>
2023-01-04 09:48:01 -0500

    fixed messages

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ server.get('/api/users', async (req, res) => {
         const users = await User.find();
         res.status(200).json(users)
     } catch (err) {
-        res.status(500).json({ message: 'something went wrong' })
+        res.status(500).json({ message: 'The users information could not be retrieved' })
     }
 })
 
@@ -19,13 +19,13 @@ server.get('/api/users/:id', async (req, res) => {
         const { id } = req.params
         const user = await User.findById(id)
         if (!user) {
-            res.status(404).json( {message: `Error retrieving user with ID ${id}`})
+            res.status(404).json( {message: `The user with the specified ID does not exist`})
         } else { 
              res.status(200).json(user)
         }
        
     } catch (err) {
-        res.status(500).json({ message: `Error fetching user ${req.params.id}`})
+        res.status(500).json({ message: `The user information could not be retrieved`})
     }
 })
 
