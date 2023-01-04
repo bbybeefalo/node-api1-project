7f32069eee3999a4f7b03a2853b071d2b8dd5478
Morgan Frotten <mmfrotten@gmail.com>
2023-01-04 11:03:04 -0500

    post

--- api/server.js
+++ api/server.js
@@ -23,10 +23,25 @@ server.get('/api/users/:id', async (req, res) => {
         } else { 
              res.status(200).json(user)
         }
-       
     } catch (err) {
         res.status(500).json({ message: `The user information could not be retrieved`})
     }
 })
 
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+
+server.post('/api/users', async (req, res) => {
+    try {
+        const { name, bio } = req.body
+        if ( !name || !bio ) {
+            res.status(400).json({ message: "Please provide name and bio for the user"})
+        } else {
+            const createdUser = await User.insert({ name, bio })
+            res.status(201).json({ data: createdUser })
+        }
+ 
+    } catch (err) {
+        res.status(500).json({ message: `There was an error while saving the user to the database`})
+    }
+})
+
+module.exports = server;
